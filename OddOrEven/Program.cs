385625afe091ff385292eba48b5db85b657b18a9
Mart Kol <martin.kolesnik@tktk.ee>
2024-11-16 14:57:51 +0200

    Добавьте файлы проекта.

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter a number: ");
+int userNum = Convert.ToInt32(Console.ReadLine());  
+//paaris or paaritu
+int result = userNum % 2;
+
+if (result != 0)
+{
+    Console.WriteLine("Number is odd");
+}
+else if (result == 0)
+{
+    Console.WriteLine("Number is even");
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
